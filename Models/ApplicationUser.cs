3de0551debc6a1f7a2236d4c087f6b5fae407b05
adamkcs <adamkocsismail@gmail.com>
2025-02-20 16:41:09 +0000

    Added Identity DbContext and Model.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskPlannerAPI.Models;
+
+/// <summary>
+/// Custom identity user model.
+/// </summary>
+public class ApplicationUser : IdentityUser
+{
+    // Custom fields
+}
