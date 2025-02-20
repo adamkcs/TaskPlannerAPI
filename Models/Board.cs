a3305b9eac6398f0c9ef59a070bb2784ce14cbae
adamkcs <adamkocsismail@gmail.com>
2025-02-20 21:02:57 +0000

    Create Board model.

---
+++ Models/Board.cs
@@ -0,0 +1,7 @@
+﻿public class Board
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public List<TaskList> TaskLists { get; set; } = new();
+}
