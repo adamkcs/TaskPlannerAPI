cd259b6d25cfe0e256856f036a8b2465ff069c1a
adamkcs <adamkocsismail@gmail.com>
2025-02-20 23:33:15 +0000

    Db migration.

---
+++ Migrations/20250220233204_AddTaskLabelManyToMany.cs
@@ -0,0 +1,421 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskPlannerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskLabelManyToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Tasks_TaskItemId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskList_Board_BoardId",
+                table: "TaskList");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_AspNetUsers_AssignedUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskList_TaskListId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "LabelTaskItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskList",
+                table: "TaskList");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Label",
+                table: "Label");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Board",
+                table: "Board");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "TaskItems");
+
+            migrationBuilder.RenameTable(
+                name: "TaskList",
+                newName: "TaskLists");
+
+            migrationBuilder.RenameTable(
+                name: "Label",
+                newName: "Labels");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Board",
+                newName: "Boards");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_TaskListId",
+                table: "TaskItems",
+                newName: "IX_TaskItems_TaskListId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_AssignedUserId",
+                table: "TaskItems",
+                newName: "IX_TaskItems_AssignedUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskList_BoardId",
+                table: "TaskLists",
+                newName: "IX_TaskLists_BoardId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_UserId",
+                table: "Comments",
+                newName: "IX_Comments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_TaskItemId",
+                table: "Comments",
+                newName: "IX_Comments_TaskItemId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BoardId",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TaskItemId",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskItems",
+                table: "TaskItems",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskLists",
+                table: "TaskLists",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Boards",
+                table: "Boards",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "TaskLabel",
+                columns: table => new
+                {
+                    TaskItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LabelId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskLabel", x => new { x.TaskItemId, x.LabelId });
+                    table.ForeignKey(
+                        name: "FK_TaskLabel_Labels_LabelId",
+                        column: x => x.LabelId,
+                        principalTable: "Labels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskLabel_TaskItems_TaskItemId",
+                        column: x => x.TaskItemId,
+                        principalTable: "TaskItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_TaskItemId",
+                table: "Labels",
+                column: "TaskItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskLabel_LabelId",
+                table: "TaskLabel",
+                column: "LabelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_TaskItems_TaskItemId",
+                table: "Comments",
+                column: "TaskItemId",
+                principalTable: "TaskItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Labels_TaskItems_TaskItemId",
+                table: "Labels",
+                column: "TaskItemId",
+                principalTable: "TaskItems",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_AspNetUsers_AssignedUserId",
+                table: "TaskItems",
+                column: "AssignedUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_TaskLists_TaskListId",
+                table: "TaskItems",
+                column: "TaskListId",
+                principalTable: "TaskLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskLists_Boards_BoardId",
+                table: "TaskLists",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_TaskItems_TaskItemId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Labels_TaskItems_TaskItemId",
+                table: "Labels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_AspNetUsers_AssignedUserId",
+                table: "TaskItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_TaskLists_TaskListId",
+                table: "TaskItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskLists_Boards_BoardId",
+                table: "TaskLists");
+
+            migrationBuilder.DropTable(
+                name: "TaskLabel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskLists",
+                table: "TaskLists");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskItems",
+                table: "TaskItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Labels_TaskItemId",
+                table: "Labels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Boards",
+                table: "Boards");
+
+            migrationBuilder.DropColumn(
+                name: "BoardId",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "TaskItemId",
+                table: "Labels");
+
+            migrationBuilder.RenameTable(
+                name: "TaskLists",
+                newName: "TaskList");
+
+            migrationBuilder.RenameTable(
+                name: "TaskItems",
+                newName: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Labels",
+                newName: "Label");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Boards",
+                newName: "Board");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskLists_BoardId",
+                table: "TaskList",
+                newName: "IX_TaskList_BoardId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskItems_TaskListId",
+                table: "Tasks",
+                newName: "IX_Tasks_TaskListId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskItems_AssignedUserId",
+                table: "Tasks",
+                newName: "IX_Tasks_AssignedUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_UserId",
+                table: "Comment",
+                newName: "IX_Comment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_TaskItemId",
+                table: "Comment",
+                newName: "IX_Comment_TaskItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskList",
+                table: "TaskList",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Label",
+                table: "Label",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Board",
+                table: "Board",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "LabelTaskItem",
+                columns: table => new
+                {
+                    LabelsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TasksId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabelTaskItem", x => new { x.LabelsId, x.TasksId });
+                    table.ForeignKey(
+                        name: "FK_LabelTaskItem_Label_LabelsId",
+                        column: x => x.LabelsId,
+                        principalTable: "Label",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LabelTaskItem_Tasks_TasksId",
+                        column: x => x.TasksId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabelTaskItem_TasksId",
+                table: "LabelTaskItem",
+                column: "TasksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Tasks_TaskItemId",
+                table: "Comment",
+                column: "TaskItemId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskList_Board_BoardId",
+                table: "TaskList",
+                column: "BoardId",
+                principalTable: "Board",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_AspNetUsers_AssignedUserId",
+                table: "Tasks",
+                column: "AssignedUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskList_TaskListId",
+                table: "Tasks",
+                column: "TaskListId",
+                principalTable: "TaskList",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
