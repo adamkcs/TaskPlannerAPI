b0e39c1b2b3f10b9c6ba4f09ac2ac19e1195402a
adamkcs <adamkocsismail@gmail.com>
2025-02-20 21:07:19 +0000

    Added Comment model.

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskPlannerAPI.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; } = null!;
+        public int TaskItemId { get; set; }
+        public TaskItem TaskItem { get; set; } = null!;
+    }
+}
