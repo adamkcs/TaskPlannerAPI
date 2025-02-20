fa3b194f62818c2eaf32f8167328e6b783d62110
adamkcs <adamkocsismail@gmail.com>
2025-02-20 22:58:50 +0000

    Extend AppDbContext.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,4 +14,5 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<TaskItem> TaskItems { get; set; }
     public DbSet<TaskList> TaskLists { get; set; }
     public DbSet<Board> Boards { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 }
