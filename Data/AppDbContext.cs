6c441169ce1ee32f242c984a027608a9d38041cf
adamkcs <adamkocsismail@gmail.com>
2025-02-20 22:34:13 +0000

    Add Controller to AppDbContext.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,5 +11,6 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<TaskItem>? Tasks { get; set; }
+    public DbSet<TaskItem> TaskItems { get; set; }
+    public DbSet<TaskList> TaskLists { get; set; }
 }
