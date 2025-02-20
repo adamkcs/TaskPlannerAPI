69d5ec6265050303b536fca56dcb818fa776e389
adamkcs <adamkocsismail@gmail.com>
2025-02-20 20:33:48 +0000

    Fix in program.cs, and appsettings. Initial db migration.

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddSwaggerGen(c =>
 
 // Config EF Core with SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Config .NET identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
