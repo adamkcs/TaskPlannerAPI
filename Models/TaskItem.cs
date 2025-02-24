044593e1db9803cf93e346ab7ec404fef9965738
Adam Kocsis <akocsis@cc-ltd.com>
2025-02-24 09:38:55 +0000

    DependencyTaskItemId added.

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -9,16 +9,25 @@
         public DateTime? DueDate { get; set; }
         public int Priority { get; set; } // 1 = High, 2 = Medium, 3 = Low
         public string Status { get; set; } = "To Do"; // Can be updated dynamically
-        public bool IsCompleted { get; set } = false;
+        public bool IsCompleted { get; set; } = false;
         public bool IsArchived { get; set; } = false;
+
         public int TaskListId { get; set; }
-        public TaskList TaskList { get; set; } = null!; // Navigation Property
+        public TaskList TaskList { get; set; } = null!;
+
         public string? AssignedUserId { get; set; }
         public ApplicationUser? AssignedUser { get; set; }
-        public List<Comment>? Comments { get; set; }
+
         public List<Label>? Labels { get; set; }
         public ICollection<TaskLabel> TaskLabels { get; set; } = new List<TaskLabel>(); 
+
         public int BoardId { get; set; }
         public Board Board { get; set; }
+
+        public int? DependencyTaskId { get; set; } // Nullable for tasks without dependencies
+        public TaskItem? DependencyTask { get; set; }
+
+        public List<Comment>? Comments { get; set; }
+
     }
 }
